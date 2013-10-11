fd8c8d696fad4bfc40e49fe831acd1975bcb9d8d
atubo <err722@yahoo.com>
2013-10-10 21:09:18 -0700

    TLE on test #34.

---
+++ P035C.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.*;
+
+public class P035C {
+    private class Pair {
+        private int x;
+        private int y;
+        
+        private Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        
+        public int hashCode() {
+            return 37 * x + y;
+        }
+        
+        public boolean equals(Object other) {
+            if (other instanceof Pair) {
+                Pair otherPair = (Pair)other;
+                return x == otherPair.x && y == otherPair.y;
+            }
+            
+            return false;
+        }
+    }
+    
+    private boolean[][] visited;
+    private final int N;
+    private final int M;
+    private final int k;
+    private ArrayList<Pair> fires = new ArrayList<Pair>();
+    
+    public P035C() throws IOException {
+        Scanner sc = new Scanner(new File("input.txt"));
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+        visited = new boolean[N][M];
+        k = sc.nextInt();
+        for (int i = 0; i < k; i++) {
+            int x = sc.nextInt() - 1;
+            int y = sc.nextInt() - 1;
+            fires.add(new Pair(x, y));
+        }
+        bfs();
+    }
+    
+    private void bfs() throws IOException{
+        java.util.Queue<Pair> queue = new LinkedList<Pair>();
+        for (Pair p : fires) {
+            queue.add(p);
+            visited[p.x][p.y] = true;
+        }
+        
+        Pair last = fires.get(0);
+        while (!queue.isEmpty()) {
+            Pair p = last = queue.poll();            
+            for (Pair pn : getNeighbors(p)) {
+                if (!visited[pn.x][pn.y]) {
+                    queue.add(pn);
+                    visited[pn.x][pn.y] = true;
+                }
+            }
+        }
+        
+        PrintWriter output = new PrintWriter(new FileWriter(new File("output.txt")));
+        output.printf("%d %d\n", last.x + 1, last.y + 1);
+        output.close();
+    }
+    
+    private ArrayList<Pair> getNeighbors(Pair p) {
+        ArrayList<Pair> result = new ArrayList<Pair>();
+        if (p.x > 0)   result.add(new Pair(p.x-1, p.y));
+        if (p.x < N-1) result.add(new Pair(p.x+1, p.y));
+        if (p.y > 0)   result.add(new Pair(p.x, p.y-1));
+        if (p.y < M-1) result.add(new Pair(p.x, p.y+1));
+        
+        return result;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        P035C solution = new P035C();
+    }
+}
+        
+      

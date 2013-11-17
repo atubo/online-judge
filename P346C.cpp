9a80c4ea61acddf23935faf3e0705149fa8b299e
atubo <err722@yahoo.com>
2013-11-16 19:29:43 -0800

    Code for problem 346C.

--- P346C.cpp
+++ P346C.cpp
@@ -3,46 +3,6 @@
 #include <vector>
 using namespace std;
 
-class SegmentTreeMin {
-public:
-    SegmentTreeMin(int n, int init):data(max(3*n, 30), init), nData(n) {}
-
-    void update(int i, int value) {
-        i += nData + 1;
-        data[i] = value;
-        for (; i > 1; i >>= 1) {
-            int newVal = min(data[i], data[i^1]);
-            if (data[i>>1] == newVal) break;
-            data[i>>1] = newVal;
-        }
-    }
-
-    int findMin() const {
-        return data[1];
-    }
-private:
-    vector<int> data;
-    int nData;
-};
-
-
-#include <unordered_map>
-#include <unordered_set>
-int round_up(int x, int y) {
-    return (x + y - 1) / y;
-}
-
-void updateLadder(vector<vector<int> >& ladder,
-                  const vector<int> xList, int i) {
-    int n = ladder.size();
-    for (int j: ladder[i]) {
-        if (i + xList[j] < n) {
-            ladder[i+xList[j]].push_back(j);
-        }
-    }
-    ladder[i].clear();
-}
-
 int main() {
     int n;
     cin >> n;
@@ -59,36 +19,19 @@ int main() {
     int a, b;
     cin >> a >> b;
 
-    const int MAXVAL = 10000000;
-    SegmentTreeMin tree(n, MAXVAL);
-
-    vector<vector<int> > ladder(a-b+1, vector<int>());
-    for (int i = 0; i < n; i++) {
-        int x = xList[i];
-        int r = round_up(b, x) * x;
-        if (r > a) continue;
-        ladder[r-b].push_back(i);
-    }
-
-    int dist = 0;
-    for (int x: ladder[0]) {
-        tree.update(x, 0);
-    }
-    updateLadder(ladder, xList, 0);
-
-    for (int i = 1; i < a-b+1; i++) {
-        for (int x: ladder[i]) {
-            tree.update(x, MAXVAL);
+    int result = 0;
+    while (a > b) {
+        int imax = 1;
+        for (int i = 0; i < n; i++) {
+            if (a - a % xList[i] >= b) {
+                imax = max(imax, a % xList[i]);
+            }
         }
-        dist = min(dist, tree.findMin()) + 1;
-        for (int x: ladder[i]) {
-            tree.update(x, dist);
-        }
-        updateLadder(ladder, xList, i);
+        a -= imax;
+        result++;
+        while (n > 1 && a - a % xList[n-1] < b) n--;
     }
-
-    cout << dist << endl;
-
+    cout << result << endl;
     return 0;
 }
 

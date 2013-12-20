e13e1a6abac55f62a5242a41bd5927ad863edbca
atubo <err722@yahoo.com>
2013-12-19 22:18:34 -0800

    Code for problem 31A.

--- P031A.java
+++ P031A.java
@@ -19,6 +19,7 @@ public class P031A {
         }
     }
     
+    // brute force
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();

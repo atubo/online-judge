3ff55ed005e2b825591027c95e4ae698a324bc6e
atubo <err722@yahoo.com>
2014-03-04 22:06:56 -0800

    Initial version

---
+++ P046E.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int64_t a[n][m+1];
+    for (int i = 0; i < n; i++) {
+        a[i][0] = 0;
+        for (int j = 1; j <= m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    int sum[n][m+1];
+    for (int i = 0; i < n; i++) {
+        sum[i][0] = 0;
+        for (int j = 1; j <= m; j++) {
+            sum[i][j] = sum[i][j-1] + a[i][j];
+        }
+    }
+
+    int64_t dp[n][m+1];
+    dp[0][0] = 0;
+    for (int j = 1; j <= m; j++) {
+        dp[0][j] = sum[0][j];
+    }
+    for (int j = m-1; j >= 1; j--) {
+        dp[0][j] = max(dp[0][j], dp[0][j+1]);
+    }
+
+    for (int i = 1; i < n; i++) {
+        dp[i][0] = 0;
+        if (i % 2 == 1) {
+            for (int j = 1; j < m; j++) {
+                dp[i][j] = sum[i][j] + dp[i-1][j+1];
+            }
+            for (int j = 2; j <= m; j++) {
+                dp[i][j] = max(dp[i][j-1], dp[i][j]);
+            }
+        } else {
+            for (int j = 2; j <= m; j++) {
+                dp[i][j] = sum[i][j] + dp[i-1][j-1];
+            }
+            for (int j = m-1; j >= 1; j--) {
+                dp[i][j] = max(dp[i][j+1], dp[i][j]);
+            }
+        }
+    }
+    if (n % 2 == 1) {
+        cout << dp[n-1][1] << endl;
+    } else {
+        cout << dp[n-1][m] << endl;
+    }
+
+    return 0;
+}

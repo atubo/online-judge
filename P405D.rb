2044da6b2e2f505b74ca9a5ad6b26889089626fe
atubo <err722@yahoo.com>
2014-03-22 22:56:16 -0700

    Code for problem 405D.

--- P405D.rb
+++ P405D.rb
@@ -1,19 +1,24 @@
 require 'set'
 N = 1000000
 n = gets.to_i
-x = Set.new(gets.split.map(&:to_i))
+#x = Set.new(gets.split.map(&:to_i))
+x = gets.split.map(&:to_i).sort!
+a = []
 y = []
 pairs = 0
 for i in x
-  unless x.include?(N+1-i)
-    y << (N+1-i)
-    next
+  a[i] = 1
+end
+for i in (1..N)
+  if a[i] and a[N+1-i]
+    pairs += 1 if i <= N/2
+  elsif a[i]
+    y << (N + 1 -i)
   end
-  pairs += 1 if i <= N/2
 end
 for i in (1..N)
   break if pairs == 0
-  unless x.include?(i) or x.include?(N+1-i)
+  unless a[i] or a[N+1-i]
     y << i << (N+1-i)
     pairs -= 1
   end

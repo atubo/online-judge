09dc45fbd29bfe94a8a25689b1750c7a54e9eca7
atubo <err722@yahoo.com>
2016-10-18 22:55:38 -0700

    add template file

---
+++ _template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define READ_ARRAY(N, A) \
+    for (int i = 0; i < N; i++) {\
+        cin >> A[i];\
+    }
+
+template <typename T>
+void printVector(const vector<T>& A) {
+    for (const T& x: A) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+

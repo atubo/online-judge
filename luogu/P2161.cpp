6066b1b940f94714a5860f7815f3217f56052970
atubo <err722@yahoo.com>
2016-12-07 00:09:14 -0800

    P2161 [SHOI2009]Booking 会场预约

--- luogu/P2161.cpp
+++ luogu/P2161.cpp
@@ -1,39 +1,34 @@
 // https://www.luogu.org/problem/show?pid=2161
 // [SHOI2009]Booking 会场预约
+#include <limits.h>
 #include <cstdio>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
+#include <set>
+#include <utility>
 using namespace std;
-using namespace __gnu_pbds;
-typedef tree<int, int, less<int>, rb_tree_tag,
-        tree_order_statistics_node_update> ordered_set;
-
-void remove(ordered_set& s, int p, int q) {
-    ordered_set::iterator it = s.find_by_order(p);
-    for (int i = 0; i < q-p; i++) {
-        it = s.erase(it);
-    }
-}
+typedef pair<int, int> PII;
+typedef set<PII>::iterator Iter;
 
 int main() {
     int N;
     scanf("%d", &N);
-    ordered_set right, left;
+    set<PII> segs;
     char str[10];
     int a, b;
     for (int i = 0; i < N; i++) {
-        scanf("%s %d %d", str, &a, &b);
+        scanf("%s", str);
         if (str[0] == 'A') {
-            int l = right.order_of_key(a);
-            int r = left.order_of_key(b+1);
-            printf("%d\n", r-l);
-            remove(left, l, r);
-            remove(right, l, r);
-            left.insert(make_pair(a, 0));
-            right.insert(make_pair(b, 0));
+            scanf("%d %d", &a, &b);
+            Iter it = segs.lower_bound(make_pair(a, INT_MIN));
+            int cnt = 0;
+            while (it != segs.end()) {
+                if (it->second > b) break;
+                cnt++;
+                segs.erase(it++);
+            }
+            segs.insert(make_pair(b, a));
+            printf("%d\n", cnt);
         } else {
-            printf("%d\n", left.size());
+            printf("%d\n", segs.size());
         }
     }
 

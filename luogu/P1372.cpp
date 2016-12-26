c35c6332c4c5289086c9af0deb9a3bee3deb3f3c
atubo <err722@yahoo.com>
2016-12-26 13:19:52 -0800

    P1372 又是毕业季I

---
+++ luogu/P1372.cpp
@@ -0,0 +1,12 @@
+// https://www.luogu.org/problem/show?pid=1372
+// P1372 又是毕业季I
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    cout << n / k << endl;
+    return 0;
+}

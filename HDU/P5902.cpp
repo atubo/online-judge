76f0c9225ccaa0f447c311cb31109c8b7f533fc1
atubo <err722@yahoo.com>
2017-01-16 11:27:11 -0800

    Fix algorithm - still WA

--- HDU/P5902.cpp
+++ HDU/P5902.cpp
@@ -4,14 +4,18 @@
 #include <inttypes.h>
 #include <algorithm>
 #include <cstdio>
+#include <set>
 #include <vector>
 
 using namespace std;
+
 
 int64_t gcd(int64_t a, int64_t b) {
     if (b == 0) return a;
     return gcd(b, a%b);
 }
+
+
 
 class Solution {
 private:
@@ -27,18 +31,39 @@ public:
     }
 
     void solve() {
-        vector<int> g(N*(N-1)/2);
-        for (int i = 0, idx = 0; i < N-1; i++) {
+        typedef set<int>::const_iterator Iter;
+        set<int> gcdSet, oldGcdSet;
+        for (int i = 0; i < N-1; i++) {
             for (int j = i+1; j < N; j++) {
-                g[idx++] = gcd(A[i], A[j]);
+                int g = gcd(A[i], A[j]);
+                oldGcdSet.insert(g);
+                gcdSet.insert(g);
             }
         }
-        sort(g.begin(), g.end());
-        g.erase(unique(g.begin(), g.end()), g.end());
-        for (int i = 0; i < (int)g.size()-1; i++) {
-            printf("%d ", g[i]);
+        set<int> newGcdSet;
+        for (int i = 3; i <= N-1; i++) {
+            for (Iter it = oldGcdSet.begin();
+                 it != oldGcdSet.end(); ++it) {
+                for (int j = 0; j < N; j++) {
+                    int g = gcd(*it, A[j]);
+                    if (gcdSet.count(g) == 0) {
+                        newGcdSet.insert(g);
+                    }
+                }
+            }
+            if (newGcdSet.empty()) break;
+            swap(newGcdSet, oldGcdSet);
+            newGcdSet.clear();
+            gcdSet.insert(oldGcdSet.begin(), oldGcdSet.end());
         }
-        printf("%d\n", g.back());
+
+        vector<int> ans(gcdSet.size());
+        copy(gcdSet.begin(), gcdSet.end(), ans.begin());
+
+        for (int i = 0; i < (int)ans.size()-1; i++) {
+            printf("%d ", ans[i]);
+        }
+        printf("%d\n", ans.back());
     }
 };
 

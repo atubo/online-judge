bc8e5a7231d4fd289314171d44bf9d788ec6cdae
atubo <err722@yahoo.com>
2017-02-04 14:03:07 -0800

    [Luogu 1156] 垃圾陷阱

--- luogu/P1156.cpp
+++ luogu/P1156.cpp
@@ -7,53 +7,60 @@ using namespace std;
 typedef pair<int, int> PII;
 
 const int MAXT = 1010;
-const int MAXD = 110;
-int dp[MAXD];
+int dp[130];
 int D, G;
 const int MINF = -1e9;
 
 int main() {
     scanf("%d %d", &D, &G);
-    vector<PII> trash(MAXT);
+    vector<vector<PII> > trash(MAXT);
     int maxt = 0;
     for (int i = 0; i < G; i++) {
         int t, f, h;
         scanf("%d %d %d", &t, &f, &h);
-        trash[t].first += f;
-        trash[t].second += h;
+        trash[t].push_back(make_pair(f, h));
         maxt = max(maxt, t);
     }
 
+    int MAXD = D + 25;
+
     dp[0] = 10;
-    for (int i = 1; i <= D; i++) {
+    for (int i = 1; i <= MAXD; i++) {
         dp[i] = MINF;
     }
 
     int ans = -1;
     int last = 0;
     for (int t = 1; t <= maxt; t++) {
-        int f = trash[t].first;
-        int h = trash[t].second;
-        bool survived = false;
-        for (int d = D; d >= 0; d--) {
+        for (int d = MAXD; d >= 0; d--) {
             dp[d]--;
             if (dp[d] < 0) dp[d] = MINF;
-            else dp[d] += f;
-            if (d >= h && dp[d-h] > 0) {
-                dp[d] = max(dp[d], dp[d-h]-1);
+        }
+
+        for (int g = 0; g < (int)trash[t].size(); g++) {
+            int f = trash[t][g].first;
+            int h = trash[t][g].second;
+
+            for (int d = MAXD; d >= 0; d--) {
+                if (dp[d] >= 0) dp[d] += f;
+                if (d >= h && dp[d-h] >= 0) {
+                    dp[d] = max(dp[d], dp[d-h]);
+                }
             }
-            if (dp[d] >= 0) survived = true;
         }
-        if (dp[D] >= 0) {
-            ans = t;
-            break;
+
+        for (int d = MAXD; d >= D; d--) {
+            if (dp[d] >= 0) {
+                ans = t;
+                goto OUT;
+            }
         }
-        if (survived) {
-            last = t;
-        } else {
-            break;
-        }
+
+        if (dp[0] >= 0) last = t;
+        else break;
     }
+
+OUT:
 
     if (last == maxt) last += dp[0];
 

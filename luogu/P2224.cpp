9af20e3a3d2581f79c746e5bb6725862ea239cb0
atubo <err722@yahoo.com>
2017-02-07 22:25:03 -0800

    Some optimization - still TLE

--- luogu/P2224.cpp
+++ luogu/P2224.cpp
@@ -7,21 +7,46 @@ using namespace std;
 int dp[30010];
 int mx;
 
+int readInt () {
+	bool minus = false;
+	int result = 0;
+	char ch;
+	ch = getchar();
+	while (true) {
+		if (ch == '-') break;
+		if (ch >= '0' && ch <= '9') break;
+		ch = getchar();
+	}
+	if (ch == '-') minus = true; else result = ch-'0';
+	while (true) {
+		ch = getchar();
+		if (ch < '0' || ch > '9') break;
+		result = result*10 + (ch - '0');
+	}
+	if (minus)
+		return -result;
+	else
+		return result;
+}
+
 int main() {
     int N;
-    scanf("%d", &N);
+    //scanf("%d", &N);
+    N = readInt();
     memset(dp, 0x7F, sizeof(dp));
     dp[0] = 0;
     for (int i = 0; i < N; i++) {
         int a, b, c;
-        scanf("%d %d %d", &a, &b, &c);
+        //scanf("%d %d %d", &a, &b, &c);
+        a = readInt();
+        b = readInt();
+        c = readInt();
         mx += max(a, c);
         for (int j = mx; j >= 0; j--) {
-            int tmp = 0x7F7F7F7F;
-            if (a && j >= a) tmp = min(tmp, dp[j-a]);
-            if (b) tmp = min(tmp, dp[j] + b);
-            if (c && j >= c) tmp = min(tmp, dp[j-c]+c);
-            dp[j] = tmp;
+            if (b) dp[j] += b;
+            else dp[j] = 0x7F7F7F7F;
+            if (a && j >= a) dp[j] = min(dp[j], dp[j-a]);
+            if (c && j >= c) dp[j] = min(dp[j], dp[j-c]+c);
         }
     }
 

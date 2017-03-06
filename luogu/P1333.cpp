60f4acba48eb2bd8f7d5218150b2e1a93a292a67
atubo <err722@yahoo.com>
2017-03-05 22:27:52 -0800

    Some optimizations

--- luogu/P1333.cpp
+++ luogu/P1333.cpp
@@ -3,13 +3,12 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-using Adj = vector<vector<int> >;
 using PII = pair<int, int>;
 
 map<int64_t, int> toId;
-Adj adj;
 int N;
 int SZ;
+int deg[250010];
 
 const int BUFSIZE = 24*250000;
 char buf[BUFSIZE];
@@ -24,45 +23,69 @@ int64_t read() {
     return ret;
 }
 
-bool bfs() {
-    vector<bool> visited(N, false);
-    queue<int> q;
-    q.push(0);
-    visited[0] = true;
 
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-        for (int v: adj[u]) {
-            if (!visited[v]) {
-                q.push(v);
-                visited[v] = true;
+class UnionFind {
+private:
+    const int N;
+    vector<int> rank;
+    vector<int> parent;
+
+    void makeSet(int k) {
+        assert(0 <= k && k < N);
+
+        parent[k] = k;
+        rank[k] = 0;
+    }
+
+    void link(int u, int v) {
+        if (rank[u] > rank[v]) {
+            parent[v] = u;
+        } else {
+            parent[u] = v;
+            if (rank[u] == rank[v]) {
+                rank[v]++;
             }
         }
     }
 
-    bool ret = true;
-    for (int i = 0; i < N; i++) {
-        if (!visited[i]) {
-            ret = false;
-            break;
+public:
+    UnionFind(int n): N(n) {
+        rank.resize(N);
+        parent.resize(N);
+
+        for (int i = 0; i < N; i++) {
+            makeSet(i);
         }
     }
 
-    return ret;
-}
-
-bool solve() {
-    if (N == 0) return true;
-    bool possible = bfs();
-    if (!possible) return false;
-
-    int oddCount = 0;
-    for (int i = 0; i < N; i++) {
-        oddCount += (adj[i].size() & 1);
+    int find(int k) {
+        if (parent[k] == k) return k;
+        int root = find(parent[k]);
+        return parent[k] = root;
     }
 
+    void join(int u, int v) {
+        link(find(u), find(v));
+    }
+};
+
+bool solve(const vector<PII>& edges) {
+    UnionFind uf(N);
+    int ncomp = N;
+    for (const auto& e: edges) {
+        int u = e.first, v = e.second;
+        if (uf.find(u) != uf.find(v)) {
+            uf.join(u, v);
+            ncomp--;
+        }
+    }
+    if (ncomp > 1) return false;
+    int oddCount = 0;
+    for (int i = 0; i < N; i++) {
+        oddCount += (deg[i] & 1);
+    }
     return oddCount == 0 || oddCount == 2;
+
 }
 
 int main() {
@@ -78,18 +101,16 @@ int main() {
         if (toId.count(b) == 0) {
             toId.insert(make_pair(b, toId.size()));
         }
-        edges.push_back(make_pair(toId.at(a), toId.at(b)));
+        int u = toId[a];
+        int v = toId[b];
+        edges.push_back(make_pair(u, v));
+        deg[u]++;
+        deg[v]++;
         if (ptr - buf == SZ) break;
     }
     N = toId.size();
-    adj.resize(N);
-    for (const auto& e: edges) {
-        int u = e.first, v = e.second;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
 
-    bool possible = solve();
+    bool possible = solve(edges);
     printf("%s\n", possible ? "Possible" : "Impossible");
 
     return 0;

67515ef1f9decee3a249debef340b0a90f356b32
atubo <err722@yahoo.com>
2017-05-01 00:13:38 -0700

    [Luogu 2780] [AHOI2016初中组]游戏

--- luogu/P2780.cpp
+++ luogu/P2780.cpp
@@ -4,52 +4,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAXM = 75000;
-bool dp[260][MAXM+1];
+const int MAXM = 75001;
+using Mask = bitset<MAXM>;
+
+vector<Mask> dp(260);
 int N, K, A, B;
-int X[260];
-
-int minDist(const vector<int> &pos, int x) {
-    auto it = lower_bound(pos.begin(), pos.end(), x);
-    if (it == pos.end()) {
-        return x - pos.back();
-    }
-    if (it == pos.begin()) {
-        return pos.front() - x;
-    }
-
-    if (*it == x) return 0;
-    auto it2 = it - 1;
-    return min(*it - x, x - *it2);
-}
-
+int L[MAXM], R[MAXM];
 
 int main() {
     scanf("%d%d%d%d", &N, &K, &A, &B);
+    int s = 0, x;
+
+    dp[0].set(0);
     for (int i = 1; i <= N; i++) {
-        scanf("%d", &X[i]);
+        scanf("%d", &x);
+        for (int k = min(i, K); k > 0; k--) {
+            dp[k] |= (dp[k-1] << x);
+        }
+        s += x;
     }
 
-    dp[0][0] = true;
-    for (int i = 1; i <= N; i++) {
-        for (int m = MAXM; m >= 0; m--) {
-            for (int k = 0; k <= K; k++) {
-                if (k > 0 && X[i] <= m) {
-                    dp[k][m] = dp[k][m] || dp[k-1][m-X[i]];
-                }
-            }
+    L[A] = INT_MIN/2;
+    for (int i = A; i >= 0; i--) {
+        if (dp[K][i]) {
+            L[A] = i;
+            break;
         }
     }
-
-    vector<int> pos;
-    for (int m = 0; m <= MAXM; m++) {
-        if (dp[K][m]) pos.push_back(m);
+    for (int i = A+1; i <= B; i++) {
+        L[i] = (dp[K][i] ? i : L[i-1]);
+    }
+    R[B] = INT_MAX/2;
+    for (int i = B; i <= s; i++) {
+        if (dp[K][i]) {
+            R[B] = i;
+            break;
+        }
+    }
+    for (int i = B-1; i >= A; i--) {
+        R[i] = (dp[K][i] ? i : R[i+1]);
     }
 
     int ret = 0;
     for (int i = A; i <= B; i++) {
-        ret = max(ret, minDist(pos, i));
+        ret = max(ret, min(i-L[i], R[i]-i));
     }
+
     printf("%d\n", ret);
     return 0;
 }

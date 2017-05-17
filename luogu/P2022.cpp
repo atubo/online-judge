1451e5b77ae2846d6232bb3a3d2c21303ce06a08
atubo <err722@yahoo.com>
2017-05-16 23:59:26 -0700

    [Luogu 2022] 有趣的数

--- luogu/P2022.cpp
+++ luogu/P2022.cpp
@@ -8,24 +8,36 @@ int main() {
     int K, M;
     cin >> K >> M;
     int64_t d = 1;
-    while (K >= 10 * d) d *= 10;
+    int c = 1;
+    while (K >= 10 * d) {
+        d *= 10;
+        c++;
+    }
     int64_t cnt = 0;
     for (int i = d, j = 1; i > 0; i /= 10) {
-        cnt += K/j - i + 1;
+        cnt += K/j - i + (j > 1);
         j *= 10;
+    }
+    
+    if (K == d) {
+        cout << (M == c ? K : 0) << endl;
+        return 0;
     }
 
     if (cnt > M - 1) {
         cout << 0 << endl;
+        return 0;
+    } else if (cnt == M - 1) {
+        cout << K << endl;
         return 0;
     }
     int64_t k = K;
     while (true) {
         k *= 10;
         d *= 10;
-        if (cnt + k - d > M - 1) break;
+        if (cnt + k - d >= M - 1) break;
         cnt += k - d;
     }
-    cout << d + M - cnt - 1 << endl;
+    cout << d + M - cnt - 2 << endl;
     return 0;
 }

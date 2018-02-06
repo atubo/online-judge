f6561dfd8e077706adf298e1d95649dc2740174d
atubo <err722@yahoo.com>
2018-02-05 20:27:22 -0800

    second version - WA

--- luogu/P3171.cpp
+++ luogu/P3171.cpp
@@ -111,20 +111,132 @@ public:
     }
 };
 
+class FastDijkstra {
+public:
+    static const int64_t INF;
+
+    typedef pair<int64_t,int> PII;  // pair = (dist, vertex)
+
+    FastDijkstra(int N_): N(N_), edges(N), dist(N, INF), dad(N, -1) {}
+
+    void addEdge(int u, int v, int64_t wt) {
+        edges[u].push_back(make_pair(wt, v));
+    }
+
+    const vector<int64_t>& getDist() const {
+        return dist;
+    }
+
+    const vector<int>& getDad() const {
+        return dad;
+    }
+
+    int64_t solve(int s, int t) {
+        // use priority queue in which top element has the "smallest" priority
+        priority_queue<PII, vector<PII>, greater<PII> > Q;
+        Q.push (make_pair (0, s));
+        dist[s] = 0;
+        while (!Q.empty()){
+            PII p = Q.top();
+            if (p.second == t) break;
+            Q.pop();
+
+            int here = p.second;
+            for (vector<PII>::const_iterator it=edges[here].begin();
+                 it!=edges[here].end(); it++){
+                if (dist[here] + it->first < dist[it->second]){
+                    dist[it->second] = dist[here] + it->first;
+                    dad[it->second] = here;
+                    Q.push (make_pair (dist[it->second], it->second));
+                }
+            }
+        }
+        return dist[t];
+    }
+
+private:
+    const int N;
+    vector<vector<PII>> edges;
+    vector<int64_t> dist;
+    vector<int> dad;
+};
+
+const int64_t FastDijkstra::INF = 0x7FFFFFFFFFFFFFFF;
+
+// Note graph node is 0-indexed
+class Graph {
+    static const int MAXM = 200010;
+public:
+    struct Edge {
+        int next, to, wt;
+    };
+
+    Edge *E;
+
+    vector<int> head;
+    int eidx;
+    int N;
+
+    Graph(int N_):N(N_) {
+        E = new Edge[MAXM]{};
+        head.resize(N);
+        eidx = 0;
+
+        for (int i = 0; i < N; i++) {
+            head[i] = -1;
+        }
+    }
+
+    // assume 0-indexed and no duplication
+    void addEdge(int u, int v, int wt) {
+        E[eidx].to = v;
+        E[eidx].next = head[u];
+        E[eidx].wt = wt;
+        head[u] = eidx++;
+    }
+};
+
 int innode(int i) {return i*2;}
 int outnode(int i) {return i*2 + 1;}
 
+const int MAXN = 510;
 int N, M;
+bool vis[MAXN];
+
+void dfs(int u, Dinic &dinic, const Graph &g, const vector<int64_t> &dist) {
+    vis[u] = true;
+    if (u == N-1) return;
+    for (int eidx = g.head[u]; eidx != -1; eidx = g.E[eidx].next) {
+        int v = g.E[eidx].to;
+        if (dist[u] - dist[v] == g.E[eidx].wt) {
+            dinic.addEdge(outnode(u), innode(v), Dinic::inf);
+            if (!vis[v]) {
+                dfs(v, dinic, g, dist);
+            }
+        }
+    }
+}
+
 int main() {
     scanf("%d%d", &N, &M);
-    Dinic dinic(2*N);
+
+    Graph g(N);
+    FastDijkstra dij(N);
     for (int i = 0; i < M; i++) {
         int a, b, d;
         scanf("%d%d%d", &a, &b, &d);
         a--; b--;
-        dinic.addEdge(outnode(a), innode(b), Dinic::inf);
-        dinic.addEdge(outnode(b), innode(a), Dinic::inf);
+        dij.addEdge(a, b, d);
+        dij.addEdge(b, a, d);
+        g.addEdge(a, b, d);
+        g.addEdge(b, a, d);
     }
+    dij.solve(N-1, 0);
+    vector<int64_t> dist = dij.getDist();
+
+    Dinic dinic(2*N);
+    dfs(0, dinic, g, dist);
+
     for (int i = 0; i < N; i++) {
         int c;
         scanf("%d", &c);
@@ -135,7 +247,7 @@ int main() {
         }
     }
 
-    printf("%d\n", dinic.dinic(innode(0), outnode(N-1)));
+    printf("%d", dinic.dinic(innode(0), outnode(N-1)));
 
     return 0;
 }

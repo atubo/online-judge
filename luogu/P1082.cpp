780344989942b0cd182734c8d2109ecf37a47aa6
atubo <err722@yahoo.com>
2018-02-10 12:17:55 -0800

    [Luogu P1082] 同余方程

---
+++ luogu/P1082.cpp
@@ -0,0 +1,35 @@
+// https://www.luogu.org/problemnew/show/P1082
+// 同余方程
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// (g, x, y) that a*x + b*y = g
+tuple<int64_t, int64_t, int64_t> ext_gcd(int64_t a, int64_t b) {
+    if (b == 0) {
+        return make_tuple(a, 1, 0);
+    }
+    int64_t dp, xp, yp;
+    tie(dp, xp, yp) = ext_gcd(b, a % b);
+    return make_tuple(dp, yp, xp - a / b * yp);
+}
+
+// find x that a*x = b mod n
+int64_t mod_solve(int64_t a, int64_t b, int n) {
+    int64_t d, xp, yp;
+    tie(d, xp, yp) = ext_gcd(a, n);
+    if (b % d == 0) {
+        int64_t x0 = (xp * (b / d) % n + n) % n;
+        return x0;
+    } else {
+        abort();
+    }
+}
+
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    int x = mod_solve(a, 1, b);
+    printf("%d\n", x % b);
+    return 0;
+}

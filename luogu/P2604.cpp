38736660c3222addc756aa0b22641f392e866a3a
atubo <err722@yahoo.com>
2018-04-14 07:32:32 -0700

    [Luogu P2604] [ZJOI2010]网络扩容

---
+++ luogu/P2604.cpp
@@ -0,0 +1,301 @@
+// https://www.luogu.org/problemnew/show/P2604
+// [ZJOI2010]网络扩容
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M, K;
+
+class MincostMaxflow {
+    // NOTE
+    // 1. set up the following values
+    // 2. nodes are 0-indexed
+    // 3. MAXM must be twice the number of edges you added
+    // 4. call init() before you use it again
+public:
+    const static int INF = 1000000;
+
+    MincostMaxflow(int N_, int M_):N(N_), Q(N_) {
+        V = new Edge*[N]{};
+        ES = new Edge[2*M_]{};
+        sp = new int[N]{};
+        prev = new int[N]{};
+        path = new Edge*[N]{};
+        init();
+    }
+
+    ~MincostMaxflow() {
+        delete[] V;
+        delete[] ES;
+        delete[] sp;
+        delete[] prev;
+        delete[] path;
+    }
+
+    void addEdge(int a, int b, int cost, int capacity) {
+        Edge e1 = {V[a], 0, b, capacity, cost}, e2 = {V[b], 0, a, 0, -cost};
+        ES[++EC] = e1; V[a] = &ES[EC];
+        ES[++EC] = e2; V[b] = &ES[EC];
+        V[a]->op = V[b]; V[b]->op = V[a];
+    }
+
+    struct FlowCost {
+        int flow, cost;
+        FlowCost& operator += (const FlowCost &other) {
+            flow += other.flow;
+            cost += other.cost;
+            return *this;
+        }
+    };
+
+    // returns maxflow, mincost
+    pair<int, int> mincostFlow(int s, int t) {
+        FlowCost fc{};
+        while (SPFA(s, t)) {
+            fc += augment(t);
+        }
+        return make_pair(fc.flow, fc.cost);
+    }
+
+private:
+    int N;
+    struct Edge {
+        Edge *next, *op;
+        int t, c, v;    // node, residual, cost
+    };
+    Edge *ES;
+    Edge **V;
+    int EC = -1;
+
+    void init() {
+        EC = -1;
+        memset(V, 0, N * sizeof(Edge*));
+    }
+
+    struct Queue {
+        Queue(int N_): N(N_) {
+            Q = new int[N]{};
+            inq = new bool[N]{};
+        }
+
+        ~Queue() {
+            delete[] Q;
+            delete[] inq;
+        }
+
+        int N;
+        int *Q;
+        int QH, QL, Size;
+        bool *inq;
+
+        void ins(int v) {
+            if (++QL >= N) QL = 0;
+            Q[QL] = v;
+            inq[v] = true;
+            Size++;
+        }
+
+        int pop() {
+            int r = Q[QH];
+            inq[r] = false;
+            Size--;
+            if (++QH >= N) QH = 0;
+            return r;
+        }
+
+        void reset() {
+            memset(Q, 0, N* sizeof(int));
+            QH = Size = 0;
+            QL = -1;
+        }
+    };
+    Queue Q;
+
+
+    int *sp, *prev;
+    Edge **path;
+
+    bool SPFA(int s, int t) {
+        int u, v;
+        for (u = 0; u < N; u++) sp[u] = INF;
+        Q.reset();
+        Q.ins(s);
+        sp[s] = 0; prev[s] = -1;
+        while (Q.Size) {
+            u = Q.pop();
+            for (Edge *k = V[u]; k; k = k->next) {
+                v = k->t;
+                if (k->c > 0 && sp[u] + k->v < sp[v]) {
+                    sp[v] = sp[u] + k->v;
+                    prev[v] = u;
+                    path[v] = k;
+                    if (!Q.inq[v]) Q.ins(v);
+                }
+            }
+        }
+        return sp[t] != INF;
+    }
+
+    FlowCost augment(int t) {
+        int i, low = INF, cost = 0;
+        Edge *e;
+        for (i = t; prev[i] != -1; i = prev[i]) {
+            e = path[i];
+            if (e->c < low) low = e->c;
+        }
+        for (i = t; prev[i] != -1; i = prev[i]) {
+            e = path[i];
+            e->c -= low; e->op->c += low;
+            cost += e->v * low;
+        }
+        return FlowCost{low, cost};
+    }
+};
+
+class Dinic {
+public:
+    static const int inf = 0x3f3f3f3f;
+    struct Edge {
+        int to, next, cap, cost;
+        bool expandable;
+    };
+
+    const int N, MAXM;
+    int *head;
+    Edge *E;
+    int e;
+private:
+    int *curr;
+
+public:
+    Dinic(int N_, int M_): N(N_), MAXM(2*M_) {
+        alloc();
+        reset();
+    }
+
+    ~Dinic() {
+        dealloc();
+    }
+
+    void reset() {
+        e = 0;
+        memset(head, -1, N * sizeof(int));
+    }
+
+    void addEdge(int x, int y, int cost, int w, int rw = 0) {
+        E[e] = {y, head[x], w, cost, true};
+        head[x] = e++;
+        E[e] = {x, head[y], rw, cost, false};
+        head[y] = e++;
+    }
+
+    int dinic(int s, int t) {
+        int ans = 0;
+        while (bfs(s, t)) {
+            for (int i = 0; i < N; i++) {
+                curr[i] = head[i];
+            }
+            int k = dfs(s, inf, t);
+            if (k > 0) ans += k;
+        }
+        return ans;
+    }
+
+    int64_t solve() {
+        MincostMaxflow flow(N+2, 4*M+2);
+        const int S = N, T = N + 1;
+        flow.addEdge(S, 0, 0, K);
+        flow.addEdge(N-1, T, 0, K);
+        for (int u = 0; u < N; u++) {
+            for (int eidx = head[u]; ~eidx; eidx = E[eidx].next) {
+                int v = E[eidx].to;
+                int cap = E[eidx].cap;
+                int cost = E[eidx].cost;
+                if (cap > 0) {
+                    flow.addEdge(u, v, 0, cap);
+                }
+                if (E[eidx].expandable) {
+                    flow.addEdge(u, v, cost, inf);
+                }
+            }
+        }
+
+        auto res = flow.mincostFlow(S, T);
+
+        return res.second;
+    }
+
+private:
+    void alloc() {
+        head = new int[N]{};
+        curr = new int[N]{};
+        E    = new Edge[MAXM]{};
+        d    = new int[N]{};
+        q    = new int[N]{};
+    }
+
+    void dealloc() {
+        delete[] head;
+        delete[] curr;
+        delete[] E;
+        delete[] d;
+        delete[] q;
+    }
+
+    int *d;
+    int *q;
+
+    bool bfs(int s, int t) {
+        memset(d, -1, N * sizeof(int));
+        int front = 0, back = 0;
+        q[back++] = t;
+
+        d[t] = 0;
+        while (front != back) {
+            int u = q[front];
+            front++;
+            for (int i = head[u]; i != -1; i = E[i].next) {
+                int v = E[i].to;
+                if (d[v] == -1 && E[i^1].cap) {
+                    d[v] = d[u] + 1;
+                    q[back++] = v;
+                    if (v == s) return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    int dfs(int x, int low, int t) {
+        if (x == t || !low) return low;
+        int ret = 0;
+        for (int &i = curr[x]; i != -1; i = E[i].next) {
+            int v = E[i].to;
+            if (d[v] == d[x] - 1) {
+                int k = dfs(v, min(low-ret, E[i].cap), t);
+                if (k > 0) {
+                    E[i].cap -= k;
+                    E[i^1].cap += k;
+                    ret += k;
+                }
+            }
+        }
+        return ret;
+    }
+};
+
+
+int main() {
+    scanf("%d%d%d", &N, &M, &K);
+    Dinic dinic(N, M);
+    for (int i = 0; i < M; i++) {
+        int u, v, c, w;
+        scanf("%d%d%d%d", &u, &v, &c, &w);
+        u--; v--;
+        dinic.addEdge(u, v, w, c);
+    }
+    printf("%d ", dinic.dinic(0, N-1));
+    int64_t ans = dinic.solve();
+    printf("%lld\n", ans);
+    return 0;
+}

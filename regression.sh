426b8fe5aeb9375e28c3e6d6f9348efa2d35fe9a
atubo <err722@yahoo.com>
2018-06-03 16:39:12 -0700

    command line input of program name

--- regression.sh
+++ regression.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for f in `ls input_*.txt`; do
     bench < $f >out1.txt
-    P2486 < $f >out2.txt
+    $1 < $f >out2.txt
     diff out1.txt out2.txt
     if [ $? -eq 0 ]; then
         echo "$f passed"

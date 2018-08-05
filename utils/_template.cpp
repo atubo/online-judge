12c1d0b50c40d8da8c8b5439b8d28e82e8e954f6
atubo <err722@yahoo.com>
2018-08-05 09:42:15 -0700

    update template

--- utils/_template.cpp
+++ utils/_template.cpp
@@ -1,9 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
 #include "../utils/BinaryTree.h"
 #include "../utils/Graph.h"
 #include "../utils/LinkedList.h"
 #include "../utils/Util.h"
-#include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 public:

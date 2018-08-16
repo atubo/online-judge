cf5388aafc8b58d277ac8b86a47ad78e9b716586
atubo <err722@yahoo.com>
2018-08-16 07:06:02 -0700

    [utils] function to create random DAG

--- utils/graph.py
+++ utils/graph.py
@@ -9,3 +9,26 @@ def rand_tree_edges(size):
         ret.append((u, v))
     return ret
 
+def rand_dag(n, m):
+    """
+    nodes are 0 indexed, returns edges
+    note the graph is not necessarily connected
+    """
+    nodes = [i for i in range(n)]
+    random.shuffle(nodes)
+    s = set()
+    for _ in range(m):
+        u = randint(0, n-1)
+        v = randint(0, n-1)
+        if u > v:
+            u, v = v, u
+        while u == v or (u, v) in s:
+            u = randint(0, n-1)
+            v = randint(0, n-1)
+            if u > v:
+                u, v = v, u
+        s.add((u, v))
+    ret = set()
+    for (u, v) in s:
+        ret.add((nodes[u], nodes[v]))
+    return ret

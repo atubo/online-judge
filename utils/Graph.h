ab5d36d86a98c46113f09f226fdffa83ee339bed
atubo <err722@yahoo.com>
2018-09-16 08:03:10 -0700

    add Graph.h

--- utils/Graph.h
+++ utils/Graph.h
@@ -1,1 +1,30 @@
-../../lintcode/Graph.h
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include <vector>
+
+struct DirectedGraphNode {
+    int label;
+    std::vector<DirectedGraphNode*> neighbors;
+    DirectedGraphNode(int x): label(x) {};
+};
+
+typedef std::vector<DirectedGraphNode*> Graph;
+
+Graph readGraph() {
+    int n, m;
+    std::cin >> n >> m;
+    Graph g(n);
+    for (int i = 0; i < n; i++) {
+        g[i] = new DirectedGraphNode(i);
+    }
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        std::cin >> u >> v;
+        g[u]->neighbors.push_back(g[v]);
+    }
+    return g;
+}
+
+#endif

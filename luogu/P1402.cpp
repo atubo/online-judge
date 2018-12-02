cf96c851e3088f1899946e51f6b8949647ada183
atubo <err722@yahoo.com>
2018-12-02 07:30:04 -0800

    [Luogu P1402] 酒店之王

---
+++ luogu/P1402.cpp
@@ -0,0 +1,148 @@
+// https://www.luogu.org/problemnew/show/P1402
+// 酒店之王
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Dinic {
+public:
+    static const int inf = 0x3f3f3f3f;
+    struct Edge {
+        int to, next, cap;
+    };
+
+    const int N, MAXM;
+    int *head;
+    Edge *E;
+    int e;
+private:
+    int *curr;
+
+public:
+    Dinic(int N_, int M_): N(N_), MAXM(2*M_) {
+        alloc();
+        reset();
+    }
+
+    ~Dinic() {
+        dealloc();
+    }
+
+    void reset() {
+        e = 0;
+        memset(head, -1, N * sizeof(int));
+    }
+
+    void addEdge(int x, int y, int w, int rw = 0) {
+        E[e] = {y, head[x], w};
+        head[x] = e++;
+        E[e] = {x, head[y], rw};
+        head[y] = e++;
+    }
+
+    int dinic(int s, int t) {
+        int ans = 0;
+        while (bfs(s, t)) {
+            for (int i = 0; i < N; i++) {
+                curr[i] = head[i];
+            }
+            int k = dfs(s, inf, t);
+            if (k > 0) ans += k;
+        }
+        return ans;
+    }
+
+private:
+    void alloc() {
+        head = new int[N]{};
+        curr = new int[N]{};
+        E    = new Edge[MAXM]{};
+        d    = new int[N]{};
+        q    = new int[N]{};
+    }
+
+    void dealloc() {
+        delete[] head;
+        delete[] curr;
+        delete[] E;
+        delete[] d;
+        delete[] q;
+    }
+
+    int *d;
+    int *q;
+
+    bool bfs(int s, int t) {
+        memset(d, -1, N * sizeof(int));
+        int front = 0, back = 0;
+        q[back++] = t;
+
+        d[t] = 0;
+        while (front != back) {
+            int u = q[front];
+            front++;
+            for (int i = head[u]; i != -1; i = E[i].next) {
+                int v = E[i].to;
+                if (d[v] == -1 && E[i^1].cap) {
+                    d[v] = d[u] + 1;
+                    q[back++] = v;
+                    if (v == s) return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    int dfs(int x, int low, int t) {
+        if (x == t || !low) return low;
+        int ret = 0;
+        for (int &i = curr[x]; i != -1; i = E[i].next) {
+            int v = E[i].to;
+            if (d[v] == d[x] - 1 && E[i].cap) {
+                int k = dfs(v, min(low-ret, E[i].cap), t);
+                if (k > 0) {
+                    E[i].cap -= k;
+                    E[i^1].cap += k;
+                    ret += k;
+                }
+            }
+            // without the following line curr[x] might change
+            // prematurely, causing runtime increase
+            if (low == ret) break;
+        }
+        return ret;
+    }
+};
+
+int N, P, Q;
+
+int main() {
+    scanf("%d%d%d", &N, &P, &Q);
+    Dinic dinic(P+2*N+Q+2, P+P*N+N+N*Q+Q);
+    const int src = 0, dst = P+2*N+Q+1;
+    for (int i = 1; i <= N; i++) {
+        dinic.addEdge(P+2*i-1, P+2*i, 1);
+    }
+    for (int p = 1; p <= P; p++) {
+        dinic.addEdge(src, p, 1);
+    }
+    for (int q = 1; q <= Q; q++) {
+        dinic.addEdge(P+2*N+q, dst, 1);
+    }
+    for (int i = 1; i <= N; i++) {
+        for (int p = 1; p <= P; p++) {
+            int x;
+            scanf("%d", &x);
+            if (x) dinic.addEdge(p, P+2*i-1, 1);
+        }
+    }
+    for (int i = 1; i <= N; i++) {
+        for (int q = 1; q <= Q; q++) {
+            int x;
+            scanf("%d", &x);
+            if (x) dinic.addEdge(P+2*i, P+2*N+q, 1);
+        }
+    }
+    printf("%d\n", dinic.dinic(src, dst));
+    return 0;
+}

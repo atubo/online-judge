867a33bf3e2f18e92d0001e446a3fa99b077fdfb
atubo <err722@yahoo.com>
2019-03-04 18:43:31 -0800

    remove trailing space

--- compare_golden.sh
+++ compare_golden.sh
@@ -3,7 +3,7 @@
 for i in `ls data/*.in`; do
     golden="${i/in/out}"
     $1 < $i > out2.txt
-    diff -b $golden out2.txt 
+    diff -b $golden out2.txt
     if [ $? -eq 0 ]; then
         echo "$i pass"
     else

b3342924fa24aab1391d826b6d142c4b16ab5ccd
atubo <err722@yahoo.com>
2019-03-04 18:39:23 -0800

    script to compare with golden data

---
+++ compare_golden.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in `ls data/*.in`; do
+    golden="${i/in/out}"
+    $1 < $i > out2.txt
+    diff -b $golden out2.txt 
+    if [ $? -eq 0 ]; then
+        echo "$i pass"
+    else
+        echo "$i fail"
+    fi
+done
